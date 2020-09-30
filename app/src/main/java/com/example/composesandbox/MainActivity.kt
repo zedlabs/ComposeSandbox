9cad9bcd8da275eb21d4b013af07ef3e7f6e5b63
zohaib <zohaibansari100@gmail.com>
2020-09-30 23:46:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/composesandbox/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.composesandbox
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import com.example.composesandbox.ui.ComposeSandboxTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+            
+        }
+    }
